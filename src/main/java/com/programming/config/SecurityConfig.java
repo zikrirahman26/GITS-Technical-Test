10cf6f0d6b2b87ad582c111ef4d2a48805cb81ac
zikrirahman26 <zikrirahman014@gmail.com>
2025-01-29 13:10:21 +0700

    Commit

--- src/main/java/com/programming/config/SecurityConfig.java
+++ src/main/java/com/programming/config/SecurityConfig.java
@@ -2,31 +2,24 @@ package com.programming.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity
 public class SecurityConfig {
 
-    @SuppressWarnings("removal")
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.csrf().disable()
-                .authorizeHttpRequests()
-                .requestMatchers("/api/generate-token").permitAll()
-                .requestMatchers("/api/products/**").authenticated()
-                .and()
-                .oauth2Login()
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .build();
-    }
-}
+   @Bean
+   SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+       return http
+               .authorizeHttpRequests(auth -> {
+                   auth.anyRequest().authenticated();
+               })
+               .oauth2Login(Customizer.withDefaults())
+               .formLogin(Customizer.withDefaults())
 
+               .build();
+   }
+}
--- src/main/java/com/programming/service/AuthService.java
+++
@@ -1,6 +0,0 @@
-package com.programming.service;
-
-public interface AuthService {
-
-    String generateToken(String username);
-}
